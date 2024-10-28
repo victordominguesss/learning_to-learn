19129b543d0471fd70a3a40140eafc4ad218d2e4
Victor Domingues <123484066+victordominguesss@users.noreply.github.com>
2024-10-28 22:09:10 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home() {
+    return  <div>
+     <h1>Olá, meu amor!</h1>
+    <p>Esta é minha primeira página na web e gostaria de dedica-la a você, te amo.</p>
+    </div>
+}
+
+export default Home;
