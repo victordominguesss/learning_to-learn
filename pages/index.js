672792397f21b7fe9a674ea916481298c7a4695f
Victor Domingues <123484066+victordominguesss@users.noreply.github.com>
2024-10-29 22:20:58 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return  <div>
      <h1>Olá, meu amor!</h1>
-    <p>Esta é minha primeira página na web e gostaria de dedica-la a você, te amo.</p>
+    <p>Esta é minha primeira página na web e gostaria de dedica-la a você, te amo!!</p>
     </div>
 }
 
