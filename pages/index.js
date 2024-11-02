64bb41d82dc9a819ea59d5b5f10ac3d3203e1e7a
Victor Domingues <123484066+victordominguesss@users.noreply.github.com>
2024-11-02 01:26:15 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 function Home() {
-    return  <div>
-     <h1>Olá, meu amor!</h1>
-    <p>Esta é minha primeira página na web e gostaria de dedica-la a você, te amo!!</p>
+  return (
+    <div>
+      <h1>Olá, meu amor!</h1>
+      <p>
+        Esta é minha primeira página na web e gostaria de dedica-la a você, te
+        amo!!
+      </p>
     </div>
+  );
 }
 
 export default Home;
